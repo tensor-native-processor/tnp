3eb66f24e1424bbecaa4104d91e0e5d142fd28d3
Yize Tang <yizet@andrew.cmu.edu>
2022-11-08 14:05:24 -0500

    Add read type for MatCache

--- hw/mat/mat_cache.sv
+++ hw/mat/mat_cache.sv
@@ -1,20 +1,27 @@
 `default_nettype none
 
+typedef enum {
+    MAT_CACHE_READ_DIAG,
+    MAT_CACHE_READ_ROW,
+    MAT_CACHE_READ_COL
+} MatCacheReadType_t;
+
 module MatCache
     #(parameter WIDTH = 128,
-                DIAG_SIZE = 1 + $clog2(WIDTH),
+                WIDTH_ADDR_SIZE = 1 + $clog2(WIDTH),
                 CACHE_SIZE = 4,
                 CACHE_ADDR_SIZE = $clog2(CACHE_SIZE))
     (input logic clock,
      input logic read_enable,
      input logic write_enable,
      input logic transpose_enable,
+     input MatCacheReadType_t read_type,
      input logic [CACHE_ADDR_SIZE - 1 : 0] read_addr1,
      input logic [CACHE_ADDR_SIZE - 1 : 0] read_addr2,
-     input logic [DIAG_SIZE - 1 : 0] read_diag,
+     input logic [WIDTH_ADDR_SIZE - 1 : 0] read_param,
      input logic [CACHE_ADDR_SIZE - 1 : 0] write_addr1,
      input logic [CACHE_ADDR_SIZE - 1 : 0] write_addr2,
-     input logic [DIAG_SIZE - 1 : 0] write_diag,
+     input logic [WIDTH_ADDR_SIZE - 1 : 0] write_diag,
      input shortreal data_in[WIDTH - 1 : 0],
      output shortreal data_out[WIDTH - 1 : 0]);
 
@@ -41,17 +48,31 @@ module MatCache
         end
     endgenerate
 
+
+    shortreal diag_out[WIDTH - 1 : 0], row_out[WIDTH - 1 : 0],
+        col_out[WIDTH - 1 : 0];
+
     // Read diagonal memory
     generate
         for (i = 0;i < WIDTH;i++) begin
             always_comb begin
-                if (i <= read_diag) begin
-                    data_out[i] = cache[read_addr1][i][read_diag - i];
+                if (i <= read_param) begin
+                    diag_out[i] = cache[read_addr1][i][read_param - i];
                 end else begin
-                    data_out[i] = cache[read_addr2][i][WIDTH + read_diag - i];
+                    diag_out[i] = cache[read_addr2][i][WIDTH + read_param - i];
                 end
+                row_out[i] = cache[read_addr1][read_param][i];
+                col_out[i] = cache[read_addr1][i][read_param];
             end
         end
     endgenerate
+
+    always_comb begin
+        unique case (read_type)
+            MAT_CACHE_READ_DIAG: data_out = diag_out;
+            MAT_CACHE_READ_ROW: data_out = row_out;
+            MAT_CACHE_READ_COL: data_out = col_out;
+        endcase
+    end
 
 endmodule: MatCache
