b5735b1f38a04f223852099836adbca399f0d2a9
Yize Tang <yizet@andrew.cmu.edu>
2022-11-08 13:35:44 -0500

    Rename enum

--- hw/vec/vec_unit.sv
+++ hw/vec/vec_unit.sv
@@ -1,11 +1,11 @@
 `default_nettype none
 
 typedef enum {
-    VEC_OP_ADD,
-    VEC_OP_SUB,
-    VEC_OP_DOT,
-    VEC_OP_SCALE
-} VecOp_t;
+    VEC_UNIT_OP_ADD,
+    VEC_UNIT_OP_SUB,
+    VEC_UNIT_OP_DOT,
+    VEC_UNIT_OP_SCALE
+} VecUnitOp_t;
 
 module VecUnit
     #(parameter WIDTH = 128)
@@ -14,23 +14,23 @@ module VecUnit
      input real data_in1[WIDTH - 1 : 0],
      input real data_in2[WIDTH - 1 : 0],
      output real data_out[WIDTH - 1 : 0],
-     input VecOp_t op);
+     input VecUnitOp_t op);
 
     genvar i;
     generate
         for (i = 0;i < WIDTH;i++) begin
             always_comb begin
                 unique case (op)
-                    VEC_OP_ADD: begin
+                    VEC_UNIT_OP_ADD: begin
                         data_out[i] = data_in1[i] + data_in2[i];
                     end
-                    VEC_OP_SUB: begin
+                    VEC_UNIT_OP_SUB: begin
                         data_out[i] = data_in1[i] - data_in2[i];
                     end
-                    VEC_OP_DOT: begin
+                    VEC_UNIT_OP_DOT: begin
                         data_out[i] = data_in1[i] * data_in2[i];
                     end
-                    VEC_OP_SCALE: begin
+                    VEC_UNIT_OP_SCALE: begin
                         data_out[i] = data_in1[i] * data_k;
                     end
                 endcase
