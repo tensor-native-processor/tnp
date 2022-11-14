3709f21cf142c5bd6357c8208ceb71e6183f1879
hyabc <yizetang@hotmail.com>
2022-11-14 14:40:45 -0500

    Add make run

--- sw/mat_asm/mat_asm.cpp
+++ sw/mat_asm/mat_asm.cpp
@@ -9,6 +9,9 @@ int main() {
 
     MatProgram prog;
     prog.append(inst);
+	inst.Md = 89;
+	inst.op = MatInstruction::MULTIPLY;
+    prog.append(inst);
     std::cout << prog.toText() << std::endl;
 
     return 0;
