bc71b5d8d974f86d7ce686f98f60cacb261b5a61
Tom Tang <yizetang@hotmail.com>
2022-11-23 20:38:41 -0500

    Add more test to vector communicate file

--- sw/example/vec_comm.cpp
+++ sw/example/vec_comm.cpp
@@ -10,12 +10,15 @@ int main() {
     for (int i = 0;i < 16;i++) B[i] = rand() % 256;
     for (int i = 0;i < 16;i++) C[i] = A[i] + B[i];
 
-    std::ofstream dm("data_mem0.txt");
+    std::ofstream dm0("data_mem0.txt");
     // Starting at addr 0
-    for (int i = 0;i < 16;i++) dm << A[i] << std::endl;
+    for (int i = 0;i < 16;i++) dm0 << A[i] << std::endl;
     // Starting at addr 16
-    for (int i = 0;i < 16;i++) dm << B[i] << std::endl;
-    dm.close();
+    for (int i = 0;i < 16;i++) dm0 << B[i] << std::endl;
+    dm0.close();
+
+    std::ofstream dm1("data_mem1.txt");
+    dm1.close();
 
     std::ofstream ans("ans.txt");
     for (int i = 0;i < 16;i++) ans << C[i] << std::endl;
@@ -24,6 +27,8 @@ int main() {
     // Start instruction mem
     VecCoreProgram prog0, prog1;
     VecCoreInst inst;
+
+    // Core 0
 
     // Load memory
     inst.opcode = VecCoreInstDefn::LOAD_VEC;
@@ -49,9 +54,23 @@ int main() {
     inst.operands[VecCoreInstDefn::V1] = 3;
     prog0.append(inst);
 
+    // receive 5 from core 1
+    inst.opcode = VecCoreInstDefn::RECV_VEC;
+    inst.operands[VecCoreInstDefn::CORE_IDX] = 1;
+    inst.operands[VecCoreInstDefn::V1] = 5;
+    prog0.append(inst);
+
+    // store 5 to memory
+    inst.opcode = VecCoreInstDefn::STORE_VEC;
+    inst.operands[VecCoreInstDefn::ADDR] = 0;
+    inst.operands[VecCoreInstDefn::V1] = 5;
+    prog0.append(inst);
+
     // halt
     inst.opcode = VecCoreInstDefn::HALT;
     prog0.append(inst);
+
+    // Core 1
 
     // receive 3 from core 0
     inst.opcode = VecCoreInstDefn::RECV_VEC;
@@ -65,6 +84,21 @@ int main() {
     inst.operands[VecCoreInstDefn::V1] = 3;
     prog1.append(inst);
 
+    // copy 3 to 7
+    inst.opcode = VecCoreInstDefn::COPY;
+    inst.operands[VecCoreInstDefn::Vd] = 7;
+    inst.operands[VecCoreInstDefn::V1] = 3;
+    prog1.append(inst);
+
+    // send 7 to core 0
+    inst.opcode = VecCoreInstDefn::SEND_VEC;
+    inst.operands[VecCoreInstDefn::CORE_IDX] = 0;
+    inst.operands[VecCoreInstDefn::V1] = 7;
+    prog1.append(inst);
+
+    // halt
+    inst.opcode = VecCoreInstDefn::HALT;
+    prog1.append(inst);
 
     SaveProgram(prog0.toBinary(), "inst_mem0.txt");
     SaveProgram(prog1.toBinary(), "inst_mem1.txt");
