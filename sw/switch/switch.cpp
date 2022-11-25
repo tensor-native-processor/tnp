2ce6d30a818316093cb22d1f5310df1d47afc7ce
hyabc <yizetang@hotmail.com>
2022-11-25 15:46:35 -0500

    Add send/recv functions

--- sw/switch/switch.cpp
+++ sw/switch/switch.cpp
@@ -1,4 +1,36 @@
 #include "switch.h"
 
+// Init switch sim engine
 SwitchSimEngine::SwitchSimEngine(const SwitchParam& param)
-: m_param(param) {}
+: m_param(param),
+  m_transit_empty(param.core_size,
+                  std::vector<bool>(param.core_size, true)),
+  m_toggle_events()
+{}
+
+// Simulate step
+void SwitchSimEngine::simulateStep() {
+    for (auto const& [s, r] : m_toggle_events) {
+        m_transit_empty[s][r] = !m_transit_empty[s][r];
+    }
+    m_toggle_events.clear();
+}
+
+// Send and receive data
+bool SwitchSimEngine::sendRequest(size_t self, size_t core_idx) {
+    if (m_transit_empty[self][core_idx]) {
+        m_toggle_events.push_back(std::make_pair(self, core_idx));
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool SwitchSimEngine::recvRequest(size_t self, size_t core_idx) {
+    if (!m_transit_empty[core_idx][self]) {
+        m_toggle_events.push_back(std::make_pair(core_idx, self));
+        return true;
+    } else {
+        return false;
+    }
+}
--- sw/switch/switch.h
+++ sw/switch/switch.h
@@ -2,6 +2,7 @@
 #define __SWITCH_H__
 
 #include <cstddef>
+#include <vector>
 
 // Switch parameters
 struct SwitchParam {
@@ -14,8 +15,18 @@ class SwitchSimEngine {
 public:
     SwitchSimEngine(const SwitchParam&);
 
+    // Switch simulateStep should be called after core simulateStep
+    void simulateStep();
+
+    // Interface with cores
+    bool sendRequest(size_t self, size_t core_idx);
+    bool recvRequest(size_t self, size_t core_idx);
+
 private:
     SwitchParam m_param;
+
+    std::vector<std::vector<bool>> m_transit_empty;
+    std::vector<std::pair<size_t, size_t>> m_toggle_events;
 };
 
 #endif
