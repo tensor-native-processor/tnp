d2503c731cb251edda71998feb66527f190f01f5
Tom Tang <yizetang@hotmail.com>
2022-11-27 16:57:51 -0500

    Finish load modal

--- sw/onnx/model.cpp
+++ sw/onnx/model.cpp
@@ -1,6 +1,34 @@
 #include "model.h"
 #include "logging.h"
+#include "error.h"
 
 ONNXModel::ONNXModel(const std::string& filename) {
     LogInfo("Loading model " + filename);
+
+    // Find file size
+    FILE* file = fopen(filename.c_str(), "rb");
+    fseek(file, 0, SEEK_END);
+    size_t filesize = ftell(file);
+    rewind(file);
+
+    if (file == NULL) {
+        FatalError(strerror(errno));
+    }
+
+    // Read into buffer
+    void* buffer = malloc(filesize);
+    fread(buffer, filesize, 1, file);
+    fclose(file);
+
+    // Construct model object
+    bool ret = m_model.ParseFromArray(buffer, filesize);
+    if (!ret) {
+        FatalError("Cannot load model " + filename);
+    }
+
+    // Free buffer
+    free(buffer);
+
+    // Model info
+    LogInfo("IR version " + std::to_string(m_model.ir_version()));
 }
