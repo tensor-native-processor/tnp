541ef2ca01dc79b11c3ea6724c35cad6d4a5a2d1
Tom Tang <yizetang@hotmail.com>
2022-11-27 16:17:26 -0500

    Add ONNX model definition

---
+++ sw/onnx/model.h
@@ -0,0 +1,12 @@
+#ifndef __MODEL_H__
+#define __MODEL_H__
+
+#include "onnx.proto3.pb.h"
+
+// Typedefs for model
+typedef ::onnx::ModelProto ONNXModel;
+typedef ::onnx::GraphProto ONNXGraph;
+typedef ::onnx::NodeProto ONNXNode;
+
+
+#endif
