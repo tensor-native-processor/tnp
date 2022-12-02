6ea4a67e6c12cff3a71dea716e1d5d548009c503
hyabc <yizetang@hotmail.com>
2022-12-02 13:11:34 -0500

    Move Tensor type to another file

--- sw/onnx/model.h
+++ sw/onnx/model.h
@@ -3,44 +3,9 @@
 
 #include "onnx.proto3.pb.h"
 
+#include "tensor.h"
 #include <string>
 #include <vector>
-
-
-// Tensor
-class Tensor {
-public:
-    // Types
-    typedef std::vector<size_t> Index;
-    typedef std::vector<size_t> Shape;
-
-    // Constructor
-    Tensor(const ::onnx::TensorProto&);
-    Tensor(const Shape&);
-
-    Tensor(const Tensor&);
-
-    // Name (optional)
-    std::string m_name;
-
-    // Dimension
-    Shape m_shape;
-    size_t m_size;
-
-    // Value
-    std::unique_ptr<float[]> m_value;
-
-    // Locate a float from index
-    float& locate(const Index&);
-    const float& locate(const Index&) const;
-
-    // Cast this tensor through unidirectional broadcast
-    void unidirectionalBroadcast(const Shape&);
-
-private:
-    void swap(Tensor&);
-    void unidirectionalBroadcastCopy(size_t, Index&, Tensor&);
-};
 
 
 // ONNX Model
---
+++ sw/onnx/tensor.cpp
@@ -0,0 +1,166 @@
+#include "model.h"
+#include "tensor.h"
+#include "logging.h"
+#include "error.h"
+#include "operator.h"
+
+#include <queue>
+#include <fstream>
+
+// Construct Tensor from protobuf
+Tensor::Tensor(const ::onnx::TensorProto& tensor) {
+    // Name
+    m_name = tensor.name();
+
+    // Dimension
+    m_size = 1;
+    for (auto dim : tensor.dims()) {
+        m_shape.push_back(dim);
+        m_size *= dim;
+    }
+    m_value = std::make_unique<float[]>(m_size);
+    std::fill_n(m_value.get(), m_size, 0);
+
+    // Value
+    switch (tensor.data_type()) {
+    case ::onnx::TensorProto::FLOAT: {
+        if (tensor.float_data_size() != 0) {
+            // Float data
+            if ((size_t)tensor.float_data_size() > m_size) {
+                FatalError("Initializer " + m_name + " larger than dimension " + std::to_string(m_size));
+            }
+            for (size_t i = 0;i < (size_t)tensor.float_data_size();i++) {
+                m_value[i] = tensor.float_data(i);
+            }
+        } else {
+            // Raw data
+            std::string bytes = tensor.raw_data();
+            if (bytes.size() / sizeof(float) > m_size) {
+                FatalError("Initializer " + m_name + " larger than dimension " + std::to_string(m_size));
+            }
+            std::copy_n(bytes.c_str(), bytes.size(), (char*)m_value.get());
+        }
+        break;
+    }
+    case ::onnx::TensorProto::INT64: {
+        if (tensor.int64_data_size() != 0) {
+            if ((size_t)tensor.int64_data_size() > m_size) {
+                FatalError("Initializer " + m_name + " larger than dimension " + std::to_string(m_size));
+            }
+            for (size_t i = 0;i < (size_t)tensor.int64_data_size();i++) {
+                // Casted to float for now
+                m_value[i] = (float)tensor.int64_data(i);
+            }
+        } else {
+            LogWarning("Initializer tensor " + m_name + " int64 raw data");
+        }
+        break;
+    }
+    default: {
+        LogWarning("Initializer tensor " + m_name + " has unsupported type " + std::to_string(tensor.data_type()) + " and set to 0");
+    }
+    }
+}
+
+// Construct Tensor from shape
+Tensor::Tensor(const Shape& shape)
+: m_shape(shape) {
+    // Calculate size
+    m_size = 1;
+    for (auto dim : m_shape) {
+        m_size *= dim;
+    }
+    // Allocate value
+    m_value = std::make_unique<float[]>(m_size);
+    std::fill_n(m_value.get(), m_size, 0);
+}
+
+// Copy-construct tensor
+Tensor::Tensor(const Tensor& tensor)
+: m_name(tensor.m_name), m_shape(tensor.m_shape), m_size(tensor.m_size) {
+    // Allocate value
+    m_value = std::make_unique<float[]>(m_size);
+
+    // Copy from tensor
+    std::copy_n(tensor.m_value.get(), m_size, m_value.get());
+}
+
+
+// Locate an index in tensor
+const float& Tensor::locate(const Index& idx) const {
+    if (idx.size() != m_shape.size()) {
+        FatalError("Tensor index mismatch shape");
+    }
+
+    size_t decode = 0, block = 1;
+    for (ssize_t pos = m_shape.size() - 1;pos >= 0;pos--) {
+        decode += idx[pos] * block;
+        block *= m_shape[pos];
+    }
+    return m_value[decode];
+}
+float& Tensor::locate(const Index& idx) {
+    return const_cast<float&>(const_cast<const Tensor*>(this)->locate(idx));
+}
+
+// Swap with another Tensor
+void Tensor::swap(Tensor& x) {
+    std::swap(m_name, x.m_name);
+    std::swap(m_shape, x.m_shape);
+    std::swap(m_size, x.m_size);
+    std::swap(m_value, x.m_value);
+}
+
+// Unidirectional broadcast
+// https://github.com/onnx/onnx/blob/main/docs/Broadcasting.md
+void Tensor::unidirectionalBroadcast(const Shape& destShape) {
+    size_t destDim = destShape.size();
+    size_t deltaDim = destDim - m_shape.size();
+    if (deltaDim < 0) {
+        FatalError("Tensor u-broadcast dim inv");
+    }
+    // Prepend 1 to m_shape (m_size is the same)
+    if (deltaDim > 0) {
+        m_shape.insert(m_shape.begin(), deltaDim, 1);
+    }
+
+    // Validate shape
+    bool sameShape = true;
+    for (size_t d = 0;d < deltaDim;d++) {
+        if (m_shape[d] != destShape[d]) {
+            sameShape = false;
+        }
+    }
+    if (sameShape) {
+        return;
+    }
+
+    // Copy tensor
+    Tensor outTensor(destShape);
+    Index progIdx(destDim, 0);
+    unidirectionalBroadcastCopy(0, progIdx, outTensor);
+    swap(outTensor);
+}
+void Tensor::unidirectionalBroadcastCopy(size_t x, Index& progIdx, Tensor& outTensor) {
+    if (x == progIdx.size()) {
+        // Copy vector
+        Index origIdx = progIdx;
+        for (size_t d = 0;d < progIdx.size();d++) {
+            if (origIdx[d] >= m_shape[d]) {
+                // Override shape
+                if (m_shape[d] == 1) {
+                    origIdx[d] = 0;
+                } else {
+                    FatalError("Tensor u-broadcast shape err");
+                }
+            }
+        }
+        outTensor.locate(progIdx) = locate(origIdx);
+        return;
+    }
+    for (size_t i = 0;i < outTensor.m_shape[x];i++) {
+        progIdx[x] = i;
+        unidirectionalBroadcastCopy(x + 1, progIdx, outTensor);
+        progIdx[x] = 0;
+    }
+}
