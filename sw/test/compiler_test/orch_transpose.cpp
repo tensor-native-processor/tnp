f5d3348e10a4a82db68e54e4224c755eea0a2676
hyabc <yizetang@hotmail.com>
2022-12-08 21:15:29 -0500

    Print reference output

--- sw/test/compiler_test/orch_transpose.cpp
+++ sw/test/compiler_test/orch_transpose.cpp
@@ -3,11 +3,13 @@
 
 #include <cstring>
 #include <iomanip>
+#include <iostream>
 #include <fstream>
 
 int main() {
+    const size_t width = 16;
     Orchestrator orch(OrchestratorParam{
-        .width = 16,
+        .width = width,
         .matCacheSize = 4096,
         .matCoreCount = 4,
         .vecCacheSize = 4096,
@@ -23,25 +25,40 @@ int main() {
         }
     }
 
-    // Comparsion output
-    std::ofstream ans("ans.txt");
-    ans << std::setprecision(6) << std::fixed;
-    for (size_t j = 0;j < m;j++) {
-        for (size_t i = 0;i < n;i++) {
-            ans << d1.locate({i, j}) << "\n";
-        }
-    }
-    ans.close();
-
-
     // Call orchestrator
-    auto mc1 = d1.toMatrixConstant(16);
+    auto mc1 = d1.toMatrixConstant(width);
     auto handle1 = orch.dataMatrixAllocate(mc1.m_shape);
     orch.dataMatrixLoadConstant(handle1, mc1);
 
     auto handle2 = orch.arithmeticTranspose(handle1);
 
     auto res = orch.dataMatrixStoreResult(handle2);
+
+
+    // Reference output
+    std::ofstream ans("ans.txt");
+    ans << std::setprecision(6) << std::fixed;
+    for (size_t j = 0;j < mc1.m_shape.y * width;j++) {
+        for (size_t i = 0;i < mc1.m_shape.x * width;i++) {
+            if (i < n && j < m) {
+                ans << d1.locate({i, j}) << "\n";
+            } else {
+                ans << 0.0f << "\n";
+            }
+        }
+    }
+    ans.close();
+
+
+    // Print hint to check output
+    std::ofstream hint("hint.txt");
+    std::cout << res.m_shape.x << ", " << res.m_shape.y << "\n";
+    for (size_t i = 0;i < res.m_shape.x * width;i++) {
+        for (size_t j = 0;j < res.m_shape.y * width;j++) {
+            hint << res.m_dataAddr[i / width][j / width] + width * (i % width) + j % width << "\n";
+        }
+    }
+    hint.close();
 
     orch.compile();
 
