ff272a71c39f5fbefce976593c2c46e715f40412
Tom Tang <yizetang@hotmail.com>
2022-12-08 20:25:25 -0500

    Transpose test program

---
+++ sw/test/compiler_test/orch_transpose.cpp
@@ -0,0 +1,34 @@
+#include "orchestration.h"
+#include "tensor.h"
+
+#include <cstring>
+
+int main() {
+    Orchestrator orch(OrchestratorParam{
+        .width = 16,
+        .matCacheSize = 256,
+        .matCoreCount = 4,
+        .vecCacheSize = 256,
+        .vecCoreCount = 4
+    });
+
+    size_t n = 56, m = 4038;
+    Tensor d1({n, m});
+
+    for (size_t i = 0;i < n;i++) {
+        for (size_t j = 0;j < m;j++) {
+            d1.locate({i, j}) = rand() % 256;
+        }
+    }
+
+
+    auto mc1 = d1.toMatrixConstant;
+    auto handle1 = orch.dataMatrixAllocate(mc1.m_shape);
+    orch.dataMatrixLoadConstant(handle1, mc1);
+
+    auto handle2 = orch.arithmeticTranspose(handle1);
+
+    orch.compile();
+
+    return 0;
+}
