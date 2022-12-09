2917661d19168424ea23aaba39b7f1d2d3768500
hyabc <yizetang@hotmail.com>
2022-12-08 21:01:18 -0500

    Add vector program disassembler

---
+++ sw/simulation/vec_disas.cpp
@@ -0,0 +1,16 @@
+#include "vec_program.h"
+#include "error.h"
+
+#include <cstdio>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        FatalError("Insufficient argc");
+    }
+    VecCoreProgram prog;
+    prog.fromBinary(LoadProgram(argv[1]));
+
+    std::cout << prog.toText() << std::endl;
+    return 0;
+}
