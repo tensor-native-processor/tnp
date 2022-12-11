e4216afc549f8408cc30790d70367ea421ce14d3
hyabc <yizetang@hotmail.com>
2022-12-10 21:11:18 -0500

    Remove newline from opcode

--- sw/example/vec_add.cpp
+++ sw/example/vec_add.cpp
@@ -25,40 +25,31 @@ int main() {
     VecCoreProgram prog;
 
     // Load memory
-    prog.append({
-        VecCoreInstDefn::LOAD_VEC, {
-            {VecCoreInstDefn::ADDR, 0},
-            {VecCoreInstDefn::V1, 5}
-        }
-    });
-    prog.append({
-        VecCoreInstDefn::LOAD_VEC, {
-            {VecCoreInstDefn::ADDR, 16},
-            {VecCoreInstDefn::V1, 6}
-        }
-    });
+    prog.append({VecCoreInstDefn::LOAD_VEC, {
+        {VecCoreInstDefn::ADDR, 0},
+        {VecCoreInstDefn::V1, 5}
+    }});
+    prog.append({VecCoreInstDefn::LOAD_VEC, {
+        {VecCoreInstDefn::ADDR, 16},
+        {VecCoreInstDefn::V1, 6}
+    }});
 
     // Add two vectors
-    prog.append({
-        VecCoreInstDefn::ADD, {
-            {VecCoreInstDefn::Vd, 3},
-            {VecCoreInstDefn::V1, 6},
-            {VecCoreInstDefn::V2, 5}
-        }
-    });
+    prog.append({VecCoreInstDefn::ADD, {
+        {VecCoreInstDefn::Vd, 3},
+        {VecCoreInstDefn::V1, 6},
+        {VecCoreInstDefn::V2, 5}
+    }});
 
     // store 3 to memory
-    prog.append({
-        VecCoreInstDefn::STORE_VEC, {
-            {VecCoreInstDefn::ADDR, 0},
-            {VecCoreInstDefn::V1, 3}
-        }
-    });
+    prog.append({VecCoreInstDefn::STORE_VEC, {
+        {VecCoreInstDefn::ADDR, 0},
+        {VecCoreInstDefn::V1, 3}
+    }});
 
     // halt
-    prog.append({
-        VecCoreInstDefn::HALT, {}
-    });
+    prog.append({VecCoreInstDefn::HALT, {
+    }});
 
     TNPProgramBinary bin = prog.toBinary();
     SaveProgram(bin, "inst_mem.txt");
