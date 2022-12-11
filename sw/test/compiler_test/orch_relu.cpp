5787406f2323d4d698f117b51cba14cd057079aa
hyabc <yizetang@hotmail.com>
2022-12-11 15:49:55 -0500

    Change hint format

--- sw/test/compiler_test/orch_relu.cpp
+++ sw/test/compiler_test/orch_relu.cpp
@@ -55,11 +55,14 @@ int main() {
 
     // Print hint to check output
     std::ofstream hint("hint.txt");
+    hint << res.m_coreIdx;
     for (size_t i = 0;i < res.m_shape.x * width;i++) {
         for (size_t j = 0;j < res.m_shape.y * width;j++) {
-            hint << res.m_dataAddr[i / width][j / width] + width * (i % width) + j % width << "\n";
+            hint << "," << res.m_dataAddr[i / width][j / width] + width * (i % width) + j % width;
         }
     }
+    hint << "\n";
+
     hint.close();
 
     orch.compile();
