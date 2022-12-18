cd6a9c76e4edcb460d08e96622d70de6acf3fc68
OscarWang114 <chihhaowang.oscar@gmail.com>
2022-12-17 20:19:50 -0500

    Adjust benchmark.cpp

--- sw/compiler/benchmark.cpp
+++ sw/compiler/benchmark.cpp
@@ -6,32 +6,24 @@
 int main(int argc, char *argv[]) {
     std::vector<std::vector<float>> matA;
     std::vector<std::vector<float>> matB;
+    
+    int w = 64;
+    bool useSingleCore = true;
+    // useSingleCore = false;
 
-    /*
-    (31, 31) * (31, 17) took 2226 cycles (CPU time 49.670s)
-    (64, 128) * (128, 10) single 8610 cycles (CPU time 178.530s) multi 5164 cycles (CPU time 429s)
-    (64, 128) * (128, 10) took 8808 cycles if set MAT_REG_SIZE to 64 (CPU time 180.360s)
-    (256, 256) * (256, 256) multi 279812 single 1154062
-    */
-    // 64 * 128
-    for (int i = 0; i < 64; i++) {
+    for (int i = 0; i < w; i++) {
         std::vector<float> row;
-        for (int j = 0; j < 128; j++) {
-            int num = j + 1;
-            if (i >= 8) num *= 2;
-            // float num = float(rand() % 65536 - 65536 / 2) / (65536 / 2);
+        for (int j = 0; j < w; j++) {
+            float num = float(rand() % 65536 - 65536 / 2) / (65536 / 2);
             row.push_back(num); 
         }
         matA.push_back(row);
     }
     
-    // 128 * 10
-    for (int i = 0; i < 128; i++) {
+    for (int i = 0; i < w; i++) {
         std::vector<float> row;
-        for (int j = 0; j < 10; j++) {
-            int num = j + 1;
-            if (i >= 8) num *= 2;
-            // float num = float(rand() % 65536 - 65536 / 2) / (65536 / 2);
+        for (int j = 0; j < w; j++) {
+            float num = float(rand() % 65536 - 65536 / 2) / (65536 / 2);
             row.push_back(num); 
         }
         matB.push_back(row);
@@ -43,12 +35,11 @@ int main(int argc, char *argv[]) {
     std::vector<std::vector<float>> matC(matA.size(), std::vector<float>(matB[0].size(), 0));
     std::vector<std::vector<float>> matRef = multBruteForce(matA, matB); 
 
-    bool useSingleCore = false;
-
     if (useSingleCore) {
         singleCore(matA, matB, matC, matRef); 
     } else {
         multiCore(matA, matB, matC, matRef); 
-    }   
+    }
+    
     return 0;
 }
