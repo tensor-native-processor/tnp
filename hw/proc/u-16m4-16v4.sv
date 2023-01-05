41900e24e55fb46bc45beb221162cabacc0ba85f
Tom Tang <yizetang@hotmail.com>
2023-01-05 09:21:45 -0500

    Add proc verilator

--- hw/proc/u-16m4-16v4.sv
+++ hw/proc/u-16m4-16v4.sv
@@ -30,36 +30,109 @@ module U_16M4_16U4
         .CACHE_SIZE(256),
         .INST_MEM_SIZE(65536),
         .DATA_MEM_SIZE(65536)
-	) MatDUT[MAT_CORE_COUNT-1:0](
+	)
+    MatDUT0(
         .clock, .reset,
-        .done(done[MAT_CORE_COUNT-1:0]),
-        .switch_send_ready(switch_send_ready[MAT_CORE_COUNT-1:0]),
-        .switch_send_core_idx(switch_send_core_idx[MAT_CORE_COUNT-1:0]),
-        .switch_send_data(switch_send_data[MAT_CORE_COUNT-1:0]),
-        .switch_send_ok(switch_send_ok[MAT_CORE_COUNT-1:0]),
-        .switch_recv_request(switch_recv_request[MAT_CORE_COUNT-1:0]),
-        .switch_recv_core_idx(switch_recv_core_idx[MAT_CORE_COUNT-1:0]),
-        .switch_recv_ready(switch_recv_ready[MAT_CORE_COUNT-1:0]),
-        .switch_recv_data(switch_recv_data[MAT_CORE_COUNT-1:0])
+        .done(done[0]),
+        .switch_send_ready(switch_send_ready[0]),
+        .switch_send_core_idx(switch_send_core_idx[0]),
+        .switch_send_data(switch_send_data[0]),
+        .switch_send_ok(switch_send_ok[0]),
+        .switch_recv_request(switch_recv_request[0]),
+        .switch_recv_core_idx(switch_recv_core_idx[0]),
+        .switch_recv_ready(switch_recv_ready[0]),
+        .switch_recv_data(switch_recv_data[0])
+    ),
+    MatDUT1(
+        .clock, .reset,
+        .done(done[1]),
+        .switch_send_ready(switch_send_ready[1]),
+        .switch_send_core_idx(switch_send_core_idx[1]),
+        .switch_send_data(switch_send_data[1]),
+        .switch_send_ok(switch_send_ok[1]),
+        .switch_recv_request(switch_recv_request[1]),
+        .switch_recv_core_idx(switch_recv_core_idx[1]),
+        .switch_recv_ready(switch_recv_ready[1]),
+        .switch_recv_data(switch_recv_data[1])
+    ),
+    MatDUT2(
+        .clock, .reset,
+        .done(done[2]),
+        .switch_send_ready(switch_send_ready[2]),
+        .switch_send_core_idx(switch_send_core_idx[2]),
+        .switch_send_data(switch_send_data[2]),
+        .switch_send_ok(switch_send_ok[2]),
+        .switch_recv_request(switch_recv_request[2]),
+        .switch_recv_core_idx(switch_recv_core_idx[2]),
+        .switch_recv_ready(switch_recv_ready[2]),
+        .switch_recv_data(switch_recv_data[2])
+    ),
+    MatDUT3(
+        .clock, .reset,
+        .done(done[3]),
+        .switch_send_ready(switch_send_ready[3]),
+        .switch_send_core_idx(switch_send_core_idx[3]),
+        .switch_send_data(switch_send_data[3]),
+        .switch_send_ok(switch_send_ok[3]),
+        .switch_recv_request(switch_recv_request[3]),
+        .switch_recv_core_idx(switch_recv_core_idx[3]),
+        .switch_recv_ready(switch_recv_ready[3]),
+        .switch_recv_data(switch_recv_data[3])
     );
-
     VecCore #(
         .SWITCH_WIDTH(SWITCH_WIDTH),
         .SWITCH_CORE_SIZE(SWITCH_CORE_SIZE),
         .CACHE_SIZE(256),
         .INST_MEM_SIZE(65536),
         .DATA_MEM_SIZE(65536)
-	) VecDUT[VEC_CORE_COUNT+MAT_CORE_COUNT-1:MAT_CORE_COUNT](
+	)
+    VecDUT4(
         .clock, .reset,
-        .done(done[VEC_CORE_COUNT+MAT_CORE_COUNT-1:MAT_CORE_COUNT]),
-        .switch_send_ready(switch_send_ready[VEC_CORE_COUNT+MAT_CORE_COUNT-1:MAT_CORE_COUNT]),
-        .switch_send_core_idx(switch_send_core_idx[VEC_CORE_COUNT+MAT_CORE_COUNT-1:MAT_CORE_COUNT]),
-        .switch_send_data(switch_send_data[VEC_CORE_COUNT+MAT_CORE_COUNT-1:MAT_CORE_COUNT]),
-        .switch_send_ok(switch_send_ok[VEC_CORE_COUNT+MAT_CORE_COUNT-1:MAT_CORE_COUNT]),
-        .switch_recv_request(switch_recv_request[VEC_CORE_COUNT+MAT_CORE_COUNT-1:MAT_CORE_COUNT]),
-        .switch_recv_core_idx(switch_recv_core_idx[VEC_CORE_COUNT+MAT_CORE_COUNT-1:MAT_CORE_COUNT]),
-        .switch_recv_ready(switch_recv_ready[VEC_CORE_COUNT+MAT_CORE_COUNT-1:MAT_CORE_COUNT]),
-        .switch_recv_data(switch_recv_data[VEC_CORE_COUNT+MAT_CORE_COUNT-1:MAT_CORE_COUNT])
+        .done(done[4]),
+        .switch_send_ready(switch_send_ready[4]),
+        .switch_send_core_idx(switch_send_core_idx[4]),
+        .switch_send_data(switch_send_data[4]),
+        .switch_send_ok(switch_send_ok[4]),
+        .switch_recv_request(switch_recv_request[4]),
+        .switch_recv_core_idx(switch_recv_core_idx[4]),
+        .switch_recv_ready(switch_recv_ready[4]),
+        .switch_recv_data(switch_recv_data[4])
+    ),
+    VecDUT5(
+        .clock, .reset,
+        .done(done[5]),
+        .switch_send_ready(switch_send_ready[5]),
+        .switch_send_core_idx(switch_send_core_idx[5]),
+        .switch_send_data(switch_send_data[5]),
+        .switch_send_ok(switch_send_ok[5]),
+        .switch_recv_request(switch_recv_request[5]),
+        .switch_recv_core_idx(switch_recv_core_idx[5]),
+        .switch_recv_ready(switch_recv_ready[5]),
+        .switch_recv_data(switch_recv_data[5])
+    ),
+    VecDUT6(
+        .clock, .reset,
+        .done(done[6]),
+        .switch_send_ready(switch_send_ready[6]),
+        .switch_send_core_idx(switch_send_core_idx[6]),
+        .switch_send_data(switch_send_data[6]),
+        .switch_send_ok(switch_send_ok[6]),
+        .switch_recv_request(switch_recv_request[6]),
+        .switch_recv_core_idx(switch_recv_core_idx[6]),
+        .switch_recv_ready(switch_recv_ready[6]),
+        .switch_recv_data(switch_recv_data[6])
+    ),
+    VecDUT7(
+        .clock, .reset,
+        .done(done[7]),
+        .switch_send_ready(switch_send_ready[7]),
+        .switch_send_core_idx(switch_send_core_idx[7]),
+        .switch_send_data(switch_send_data[7]),
+        .switch_send_ok(switch_send_ok[7]),
+        .switch_recv_request(switch_recv_request[7]),
+        .switch_recv_core_idx(switch_recv_core_idx[7]),
+        .switch_recv_ready(switch_recv_ready[7]),
+        .switch_recv_data(switch_recv_data[7])
     );
 
     Switch #(.WIDTH(SWITCH_WIDTH), .CORE_SIZE(SWITCH_CORE_SIZE)) switch(
@@ -88,67 +161,67 @@ module U_16M4_16U4
     integer i, data_mem_file;
     initial begin
         // Load instruction/data memory
-        $readmemb("inst_mem0.txt", MatDUT[0].inst_mem.inst_mem);
-        $readmemb("inst_mem1.txt", MatDUT[1].inst_mem.inst_mem);
-        $readmemb("inst_mem2.txt", MatDUT[2].inst_mem.inst_mem);
-        $readmemb("inst_mem3.txt", MatDUT[3].inst_mem.inst_mem);
-        $readmemb("inst_mem4.txt", VecDUT[4].inst_mem.inst_mem);
-        $readmemb("inst_mem5.txt", VecDUT[5].inst_mem.inst_mem);
-        $readmemb("inst_mem6.txt", VecDUT[6].inst_mem.inst_mem);
-        $readmemb("inst_mem7.txt", VecDUT[7].inst_mem.inst_mem);
+        $readmemb("inst_mem0.txt", MatDUT0.inst_mem.inst_mem);
+        $readmemb("inst_mem1.txt", MatDUT1.inst_mem.inst_mem);
+        $readmemb("inst_mem2.txt", MatDUT2.inst_mem.inst_mem);
+        $readmemb("inst_mem3.txt", MatDUT3.inst_mem.inst_mem);
+        $readmemb("inst_mem4.txt", VecDUT4.inst_mem.inst_mem);
+        $readmemb("inst_mem5.txt", VecDUT5.inst_mem.inst_mem);
+        $readmemb("inst_mem6.txt", VecDUT6.inst_mem.inst_mem);
+        $readmemb("inst_mem7.txt", VecDUT7.inst_mem.inst_mem);
 
         data_mem_file = $fopen("data_mem0.txt", "r");
         i = 0;
-        while ($fscanf(data_mem_file, "%f", MatDUT[0].data_mem.data_mem[i]) == 1) begin
+        while ($fscanf(data_mem_file, "%f", MatDUT0.data_mem.data_mem[i]) == 1) begin
             i++;
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("data_mem1.txt", "r");
         i = 0;
-        while ($fscanf(data_mem_file, "%f", MatDUT[1].data_mem.data_mem[i]) == 1) begin
+        while ($fscanf(data_mem_file, "%f", MatDUT1.data_mem.data_mem[i]) == 1) begin
             i++;
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("data_mem2.txt", "r");
         i = 0;
-        while ($fscanf(data_mem_file, "%f", MatDUT[2].data_mem.data_mem[i]) == 1) begin
+        while ($fscanf(data_mem_file, "%f", MatDUT2.data_mem.data_mem[i]) == 1) begin
             i++;
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("data_mem3.txt", "r");
         i = 0;
-        while ($fscanf(data_mem_file, "%f", MatDUT[3].data_mem.data_mem[i]) == 1) begin
+        while ($fscanf(data_mem_file, "%f", MatDUT3.data_mem.data_mem[i]) == 1) begin
             i++;
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("data_mem4.txt", "r");
         i = 0;
-        while ($fscanf(data_mem_file, "%f", VecDUT[4].data_mem.data_mem[i]) == 1) begin
+        while ($fscanf(data_mem_file, "%f", VecDUT4.data_mem.data_mem[i]) == 1) begin
             i++;
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("data_mem5.txt", "r");
         i = 0;
-        while ($fscanf(data_mem_file, "%f", VecDUT[5].data_mem.data_mem[i]) == 1) begin
+        while ($fscanf(data_mem_file, "%f", VecDUT5.data_mem.data_mem[i]) == 1) begin
             i++;
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("data_mem6.txt", "r");
         i = 0;
-        while ($fscanf(data_mem_file, "%f", VecDUT[6].data_mem.data_mem[i]) == 1) begin
+        while ($fscanf(data_mem_file, "%f", VecDUT6.data_mem.data_mem[i]) == 1) begin
             i++;
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("data_mem7.txt", "r");
         i = 0;
-        while ($fscanf(data_mem_file, "%f", VecDUT[7].data_mem.data_mem[i]) == 1) begin
+        while ($fscanf(data_mem_file, "%f", VecDUT7.data_mem.data_mem[i]) == 1) begin
             i++;
         end
         $fclose(data_mem_file);
@@ -169,54 +242,55 @@ module U_16M4_16U4
         #10;
 
         data_mem_file = $fopen("output0.txt", "w");
-        for (i = 0;i < MatDUT[0].DATA_MEM_SIZE;i++) begin
-            $fwrite(data_mem_file, "%f\n", MatDUT[0].data_mem.data_mem[i]);
+        for (i = 0;i < MatDUT0.DATA_MEM_SIZE;i++) begin
+            $fwrite(data_mem_file, "%f\n", MatDUT0.data_mem.data_mem[i]);
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("output1.txt", "w");
-        for (i = 0;i < MatDUT[1].DATA_MEM_SIZE;i++) begin
-            $fwrite(data_mem_file, "%f\n", MatDUT[1].data_mem.data_mem[i]);
+        for (i = 0;i < MatDUT1.DATA_MEM_SIZE;i++) begin
+            $fwrite(data_mem_file, "%f\n", MatDUT1.data_mem.data_mem[i]);
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("output2.txt", "w");
-        for (i = 0;i < MatDUT[2].DATA_MEM_SIZE;i++) begin
-            $fwrite(data_mem_file, "%f\n", MatDUT[2].data_mem.data_mem[i]);
+        for (i = 0;i < MatDUT2.DATA_MEM_SIZE;i++) begin
+            $fwrite(data_mem_file, "%f\n", MatDUT2.data_mem.data_mem[i]);
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("output3.txt", "w");
-        for (i = 0;i < MatDUT[3].DATA_MEM_SIZE;i++) begin
-            $fwrite(data_mem_file, "%f\n", MatDUT[3].data_mem.data_mem[i]);
+        for (i = 0;i < MatDUT3.DATA_MEM_SIZE;i++) begin
+            $fwrite(data_mem_file, "%f\n", MatDUT3.data_mem.data_mem[i]);
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("output4.txt", "w");
-        for (i = 0;i < VecDUT[4].DATA_MEM_SIZE;i++) begin
-            $fwrite(data_mem_file, "%f\n", VecDUT[4].data_mem.data_mem[i]);
+        for (i = 0;i < VecDUT4.DATA_MEM_SIZE;i++) begin
+            $fwrite(data_mem_file, "%f\n", VecDUT4.data_mem.data_mem[i]);
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("output5.txt", "w");
-        for (i = 0;i < VecDUT[5].DATA_MEM_SIZE;i++) begin
-            $fwrite(data_mem_file, "%f\n", VecDUT[5].data_mem.data_mem[i]);
+        for (i = 0;i < VecDUT5.DATA_MEM_SIZE;i++) begin
+            $fwrite(data_mem_file, "%f\n", VecDUT5.data_mem.data_mem[i]);
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("output6.txt", "w");
-        for (i = 0;i < VecDUT[6].DATA_MEM_SIZE;i++) begin
-            $fwrite(data_mem_file, "%f\n", VecDUT[6].data_mem.data_mem[i]);
+        for (i = 0;i < VecDUT6.DATA_MEM_SIZE;i++) begin
+            $fwrite(data_mem_file, "%f\n", VecDUT6.data_mem.data_mem[i]);
         end
         $fclose(data_mem_file);
 
         data_mem_file = $fopen("output7.txt", "w");
-        for (i = 0;i < VecDUT[7].DATA_MEM_SIZE;i++) begin
-            $fwrite(data_mem_file, "%f\n", VecDUT[7].data_mem.data_mem[i]);
+        for (i = 0;i < VecDUT7.DATA_MEM_SIZE;i++) begin
+            $fwrite(data_mem_file, "%f\n", VecDUT7.data_mem.data_mem[i]);
         end
         $fclose(data_mem_file);
 
 
+        $display("Finished at time %0t", $time);
         $finish;
     end
 
